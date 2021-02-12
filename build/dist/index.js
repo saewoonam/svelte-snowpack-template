e2521121aad6e9ebbad69cb3fba2eef09d9ecee9
Sae Woo Nam <nams@nist.gov>
2021-02-12 11:07:00 -0700

    tweak stuff

--- build/dist/index.js
+++ build/dist/index.js
@@ -1,6 +1,3 @@
-import * as __SNOWPACK_ENV__ from '../_snowpack/env.js';
-import.meta.env = __SNOWPACK_ENV__;
-
 /* Add JavaScript code here! */
 console.log('Hello World! You did! Welcome to Snowpack :D');
 import App from "./App.svelte.js";
@@ -10,12 +7,3 @@ let app = new App({
 });
 
 export default app;
-
-// Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
-// Learn more: https://www.snowpack.dev/concepts/hot-module-replacement
-if (undefined /* [snowpack] import.meta.hot */ ) {
-  undefined /* [snowpack] import.meta.hot */ .accept();
-  undefined /* [snowpack] import.meta.hot */ .dispose(() => {
-    app.$destroy();
-  });
-}
